91ef853c5fca5a2866870248bf0a8a4f5a21f915
simplysky <simplysky@simplyskydemacbook-pro.local>
2016-04-21 15:40:54 +0800

    add lib

---
+++ MyWeiBo/MyWeiBo/TabBarButton.h
@@ -0,0 +1,13 @@
+//
+//  TabBarButton.h
+//  QuWanWang
+//
+//  Created by tony on 15/4/2.
+//  Copyright (c) 2015年 tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarButton : UIButton
+@property(nonatomic,strong)UILabel *title;
+@end
---
+++ MyWeiBo/MyWeiBo/TabBarButton.m
@@ -0,0 +1,21 @@
+//
+//  TabBarButton.m
+//  QuWanWang
+//
+//  Created by tony on 15/4/2.
+//  Copyright (c) 2015年 tony. All rights reserved.
+//
+
+#import "TabBarButton.h"
+
+@implementation TabBarButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
